89b93f59ff40ed244278d21f84555d321c7beb62
tarek <tarekmonjur@gmail.com>
2017-02-05 11:26:03 +0600

    web.php file HEAD remove

--- routes/web.php
+++ routes/web.php
@@ -1,44 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-/************************************ ..Setup System Route.. ***************************************/
-=======
-/************ ...HRMS Login Route... ****************/
-Route::group(['prefix'=>'/','namespace'=>'Auth'], function(){
-	Route::get('login','LoginController@showLoginForm');
-	Route::post('login', 'LoginController@login');
-	Route::post('logout', 'LoginController@logout');
-
-	//Password Reset Routes...
-	Route::get('password/reset', 'ForgotPasswordController@showLinkRequestForm');
-	Route::post('password/reset', 'ResetPasswordController@reset');
-	Route::get('password/reset/{token?}', 'ResetPasswordController@showResetForm');
-	Route::post('password/email', 'ForgotPasswordController@sendResetLinkEmail');
-});
-
-/***************** ...HRMS Dashboard Routes... ******************/
-Route::group(['prefix'=>'/'], function(){
-	Route::get('/','DashboardController@index');
-});
-
-
-
-// Registration Routes...
-// Route::get('register', 'Auth\RegisterController@showRegistrationForm');
-// Route::post('register', 'Auth\RegisterController@register');
-
-
-// Password Reset Routes...
-// Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
-// Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
-// Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
-// Route::post('password/reset', 'Auth\ResetPasswordController@reset');
-
-
-/***************** ..Setup System Route... *******************/
->>>>>>> 4fc99536c6521bee4fd748076a9245524116a3a0
-
-
 /***************** ...Setup Login Routes... ******************/
 Route::group(['prefix'=>'setup','namespace'=>'Setup\Auth'], function(){
 	Route::get('login', 'LoginController@showLoginForm');
