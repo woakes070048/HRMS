c95385c586cc4096b12e4e9b1ac646b312812122
Tarek Monjur <tarekmonjur@gmail.com>
2017-03-04 22:56:30 +0600

    Update 2017_02_14_142556_create_basic_salary_info_table.php

--- database/migrations/hrms/2017_02_14_142556_create_basic_salary_info_table.php
+++ database/migrations/hrms/2017_02_14_142556_create_basic_salary_info_table.php
@@ -18,7 +18,7 @@ class CreateBasicSalaryInfoTable extends Migration
             $table->string('salary_info_name', 45)->unique(); 
             $table->float('salary_info_amount', 9, 2);
             $table->boolean('salary_info_amount_status')->default(0)->comment='0=percent, 1=amount-tk';
-            $table->enum('salary_info_type', ['Allowance', 'Deduct'])->comment='allowance add with basic, deduct minus from basic salry';
+            $table->enum('salary_info_type', ['allowance', 'deduction'])->comment='allowance add with basic, deduction minus from basic salary';
             $table->timestamps();
         });
     }
